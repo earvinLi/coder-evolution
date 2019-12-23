8f0565f151013f4128960ded8557b64872c67d6e
earvinli <earvin.tli@gmail.com>
2019-12-23 12:22:34 +0800

    [App Header] feat: create a basic 'AppHeader'

--- src/AppHeader/AppHeader.js
+++ src/AppHeader/AppHeader.js
@@ -1,7 +1,18 @@
 // External Dependencies
 import React from 'react';
 
+// Material-UI Dependencies
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
+
 // Component Definition
-const AppHeader = () => <div>I'm a header of this app.</div>;
+const AppHeader = () => (
+  <AppBar position="static">
+    <Toolbar>
+      <Typography variant="h6">Coder Evolution</Typography>
+    </Toolbar>
+  </AppBar>
+);
 
 export default AppHeader;
