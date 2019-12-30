79a3aefe932277d73a95e512190608c0d1f3f150
earvinli <earvin.tli@gmail.com>
2019-12-30 19:50:29 +0800

    [Article] feat: try to send article to 'DynamoDB' through 'Lambda'

--- src/Article/actions/ArticleEditorAction.js
+++ src/Article/actions/ArticleEditorAction.js
@@ -8,9 +8,17 @@ import {
 
 export const closeArticleEditor = createActionCreator(CLOSE_ARTICLE_EDITOR);
 
-export const saveArticleText = (articleText) => (dispatch, getState) => {
+export const saveArticleText = (articleText) => async (dispatch, getState) => {
   const { currentArticle } = getState().UI.Sidebar;
   window.localStorage.setItem(currentArticle, articleText);
+  const data = { name: articleText };
+  await fetch('https://wso04ok6i9.execute-api.ap-northeast-1.amazonaws.com/prod/employees', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
 
   dispatch({
     type: SAVE_ARTICLE_TEXT,
